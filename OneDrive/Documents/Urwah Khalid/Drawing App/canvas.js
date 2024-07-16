b6189ed508d7eea3fac9c8f34ad1a1c7475eaaf8
urwahkhalid00 <urwahkhalid00@gmail.com>
2024-07-16 16:55:58 +0500

    main

---
+++ OneDrive/Documents/Urwah Khalid/Drawing App/canvas.js
@@ -0,0 +1,63 @@
+const canvas = document.getElementById("canvas");
+const body = document.querySelector("body");
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+var thecolor = '';
+let prevX = null;
+let prevY = null;
+let draw = false;
+
+body.style.backgroundColor = "#fffffff";
+var theinput = document.getElementById("favclr");
+theinput.addEventListener("input" , function () {
+    thecolor = theinput.value;
+    body.style.backgroundColor = thecolor;
+}, false);
+
+const ctx = canvas.getContext("2d");
+
+let clrs = document.querySelectorAll(".clr");
+clrs = Array.from(clrs);
+clrs.forEach(clr => {
+    clr.addEventListener("click", ()=>{
+        ctx.strokeStyle = clr.dataset.clr;
+    })
+})
+
+let clearBtn = document.querySelector(".clear");
+clearBtn.addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+})
+
+let saveBtn = document.querySelector(".save");
+saveBtn.addEventListener("click", () => {
+    let data = canvas.toDataURL("imag/png");
+    let a = document.createElement("a");
+    a.href = data;
+    a.download = "sketch.png";
+    a.click();
+})
+
+window.addEventListener("mousedown", (e) => draw = true);
+window.addEventListener("mouseup", (e) => draw = false);
+
+window.addEventListener("mousemove", (e) => {
+    if(prevX == null || prevY == null || !draw){
+        prevX = e.clientX;
+        prevY = e.clientY;
+        return
+    }
+
+    let currentX = e.clientX;
+    let currentY = e.clientY;
+
+    ctx.beginPath();
+    ctx.moveTo(prevX, prevY);
+    ctx.lineTo(currentX, currentY);
+    ctx.stroke();
+
+    prevX = currentX;
+    prevY = currentY;
+})
+
+
